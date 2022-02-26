8ad655b275a512a10c6e505b3c463726102a5ad7
ShalunzQ <1825661589@qq.com>
2022-02-26 13:00:44 +0800

    new

---
+++ src/newpacket/newworld.java
@@ -0,0 +1,30 @@
+package newpacket;
+
+import java.util.Scanner;
+
+public class newworld {
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		int i=0,max=0,j=0;
+		while(true){
+			int str = sc.nextInt();
+			if(str>max){
+				max=str;
+				j=0;
+			}
+			if(str==max){
+				j++;
+			}
+			if(str==0){
+				break;
+			}
+			i++;
+		}
+		if(i==0)
+			System.out.println("No input");
+		else
+			System.out.println(max+" "+j);
+	}
+
+}

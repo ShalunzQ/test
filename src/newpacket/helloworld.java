c03810ebc47b272dd42909c82c9456950cb606b3
ShalunzQ <1825661589@qq.com>
2022-03-03 10:24:55 +0800

    welcome to java

--- src/newpacket/helloworld.java
+++ src/newpacket/helloworld.java
@@ -2,6 +2,6 @@ package newpacket;
 
 public class helloworld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Programming in Java is fun!");
 	}
 }

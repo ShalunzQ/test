c5f713b6019fd08bc063d408114cfd073ffb3682
ShalunzQ <1825661589@qq.com>
2022-03-05 11:44:25 +0800

    bin

---
+++ src/newpacket/mesage.java
@@ -0,0 +1,9 @@
+package newpacket;
+import java.util.Scanner;
+public class mesage {
+    public static void main(String[] args) {
+    	Scanner input=new Scanner(System.in);
+    	double mile=input.nextDouble();
+    	double foot,inch;
+    }
+}
